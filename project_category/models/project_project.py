c82a6c0177fcc7d57d500084d7e5f7b325db2d04
hp <bhanusmitaborah@gmail.com>
2020-07-22 20:39:09 +0530

    1

--- project_category/models/project_project.py
+++ project_category/models/project_project.py
@@ -1,14 +1,3 @@
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-
-from odoo import fields, models
-
-
-class ProjectProject(models.Model):
-    _inherit = "project.project"
-
-    type_id = fields.Many2one(
-        comodel_name="project.type",
-        string="Type",
-        copy=False,
-        domain="[('project_ok', '=', True)]",
-    )
+print("License AGPL-3.0 or later (http://www.gnu.org/licenses/agp")
+print("ok")
+print("bye")
